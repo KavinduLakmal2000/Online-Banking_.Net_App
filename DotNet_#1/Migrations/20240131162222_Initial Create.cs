c42dd1bcd1e1a97d55ed852d660fce3065e1ccbb
Kavindu Lakmal <87576012+KavinduLakmal2000@users.noreply.github.com>
2024-02-01 12:35:57 +0530

    Add project files.

---
+++ DotNet_#1/Migrations/20240131162222_Initial Create.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNet__1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    transactionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    transactionName = table.Column<string>(type: "nvarchar(12)", nullable: false),
+                    transactionType = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    bankName = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.transactionID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
---
+++ DotNet_#1/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotNet__1.Models
+{
+	public class Transaction
+	{
+		[Key]
+        
+        public int transactionID { get; set; }
+
+		[Column(TypeName = "nvarchar(12)")]
+		[DisplayName("Transaction Name")]
+		[Required(ErrorMessage ="This field is required!")]
+		public string transactionName { get; set; }
+
+		[Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Transaction Type")]
+        [Required(ErrorMessage = "This field is required!")]
+        public string transactionType { get; set; }
+
+		[Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This field is required!")]
+		[MaxLength(12, ErrorMessage ="Maximum 12 Characters only!")]
+        public string bankName { get; set; }
+
+
+	}
+}
